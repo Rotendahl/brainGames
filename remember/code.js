87515b397f6a439f1a0e2534d835ce999ed460d1
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2016-03-09 22:29:51 +0100

    Added files for rembmer game

---
+++ remember/code.js
@@ -0,0 +1,18 @@
+var sketchProc = function(pros) {
+with (pros) {
+
+// Game settings
+var screenX = 500;
+var screenY = 700;
+size(screenX, screenY);
+
+
+var draw = function(){
+    
+}
+
+}};
+
+// Run game
+var canvas = document.getElementById("mycanvas");
+var pros = new Processing(canvas, sketchProc);
